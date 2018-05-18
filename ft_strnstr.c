f48c171d111bcd9011062db9ba014621a3fadec4
Gj STEYN <gsteyn@c1r2s3.wethinkcode.co.za>
2018-05-18 08:48:44 +0200

    Update

--- ft_strnstr.c
+++ ft_strnstr.c
@@ -6,13 +6,13 @@
 /*   By: gsteyn <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/05/18 08:41:27 by gsteyn            #+#    #+#             */
-/*   Updated: 2018/05/18 08:47:10 by gsteyn           ###   ########.fr       */
+/*   Updated: 2018/05/18 08:48:14 by gsteyn           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-char	*strnstr(const char *haystack, const char *needle, size_t len)
+char	*ft_strnstr(const char *haystack, const char *needle, size_t len)
 {
 	size_t	i;
 
