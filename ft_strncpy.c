d176d9b3d5d8da05c8a04db319a3013cf0f8fa25
GJSteyn <36443537+GJSteyn@users.noreply.github.com>
2018-05-20 17:41:53 +0200

    fixed ft_strncpy.c

--- ft_strncpy.c
+++ ft_strncpy.c
@@ -6,21 +6,26 @@
 /*   By: gsteyn <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/05/17 08:32:44 by gsteyn            #+#    #+#             */
-/*   Updated: 2018/05/17 08:37:10 by gsteyn           ###   ########.fr       */
+/*   Updated: 2018/05/20 17:38:22 by gsteyn           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-char	*strncpy(char *dst, const char *src, size_t len)
+char	*ft_strncpy(char *dst, const char *src, size_t len)
 {
 	size_t	i;
 
 	i = 0;
-	while (i < len)
+	while ((i < len) && src[i])
 	{
 		dst[i] = src[i];
 		i++;
+	}
+	if (!src[i])
+	{
+		while (i < len)
+			dst[i++] = '\0';
 	}
 	return (dst);
 }
