998499a7c23848bc447fc37c1bee0f413b65b73b
GJSteyn <36443537+GJSteyn@users.noreply.github.com>
2018-05-20 12:20:02 +0200

    Improved ft_memcpy and ft_memccpy

--- ft_memccpy.c
+++ ft_memccpy.c
@@ -6,7 +6,7 @@
 /*   By: gsteyn <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/05/16 17:03:54 by gsteyn            #+#    #+#             */
-/*   Updated: 2018/05/16 17:18:18 by gsteyn           ###   ########.fr       */
+/*   Updated: 2018/05/20 11:58:01 by gsteyn           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,15 +14,13 @@
 
 void	*ft_memccpy(void *dst, const void *src, int c, size_t n)
 {
-	unsigned char	cc;
-	size_t			i;
+	size_t	i;
 
-	cc = (unsigned char)c;
 	i = 0;
 	while (i <= n)
 	{
-		((unsigned char*)src)[i] = ((unsigned char*)src)[i];
-		if (((unsigned char*)src)[i] == cc)
+		((unsigned char*)dst)[i] = ((unsigned char*)src)[i];
+		if (((unsigned char*)src)[i] == (unsigned char)c)
 			return (dst);
 		i++;
 	}
--- ft_memcpy.c
+++ ft_memcpy.c
@@ -1,15 +1,25 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_memcpy.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: gsteyn <gsteyn@student.wethinkcode.co      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2018/05/20 10:37:51 by gsteyn            #+#    #+#             */
+/*   Updated: 2018/05/20 10:40:29 by gsteyn           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include "libft.h"
 #include <string.h>
 
 void	*ft_memcpy(void *dest, const void *src, size_t n)
 {
-	size_t	i;
-
-	i = 0;
-	while (i < n)
+	while (n--)
 	{
-		((unsigned char*)dest)[i] = ((unsigned char*)src)[i];
-		i++;
+		*((unsigned char*)dest) = *((unsigned char*)src);
+		dest++;
+		src++;
 	}
 	return (dest);
 }
