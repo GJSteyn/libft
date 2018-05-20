316787cafd44e9484c0d4ef61478ebe1661544fb
GJSteyn <36443537+GJSteyn@users.noreply.github.com>
2018-05-20 13:38:04 +0200

    ft_memcmp.c update

--- ft_memcmp.c
+++ ft_memcmp.c
@@ -6,7 +6,7 @@
 /*   By: gsteyn <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/05/17 07:41:14 by gsteyn            #+#    #+#             */
-/*   Updated: 2018/05/17 07:44:56 by gsteyn           ###   ########.fr       */
+/*   Updated: 2018/05/20 13:33:27 by gsteyn           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,9 +20,7 @@ int	ft_memcmp(const void *s1, const void *s2, size_t n)
 	while (i < n)
 	{
 		if (((unsigned char*)s1)[i] != ((unsigned char*)s2)[i])
-		{
 			return (((unsigned char*)s1)[i] - ((unsigned char*)s2)[i]);
-		}
 		i++;
 	}
 	return (0);
