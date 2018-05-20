e8ab2df9b0ecd3fb59c68ecc83c5922847f85a20
GJSteyn <36443537+GJSteyn@users.noreply.github.com>
2018-05-20 13:05:30 +0200

    Fix ft_memmove.c

--- ft_memmove.c
+++ ft_memmove.c
@@ -6,7 +6,7 @@
 /*   By: gsteyn <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/05/16 17:41:00 by gsteyn            #+#    #+#             */
-/*   Updated: 2018/05/17 07:30:58 by gsteyn           ###   ########.fr       */
+/*   Updated: 2018/05/20 13:04:31 by gsteyn           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,7 +17,8 @@ void	*ft_memmove(void *dst, const void *src, size_t len)
 	void	*temp;
 
 	temp = (unsigned char*)malloc(len);
-	temp = ft_memcpy(temp, src, len);
-	dst = ft_memcpy(dst, temp, len);
+	ft_memcpy(temp, src, len);
+	ft_memcpy(dst, temp, len);
+	free(temp);
 	return (dst);
 }
