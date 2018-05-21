9d6a7668e840aa00586ff6e57049aa7ad5fb2351
GJSteyn <6443537+GJSteyn@users.noreply.github.com>
2018-05-21 09:32:11 +0200

    ft_strncmp.c fix

--- ft_strncmp.c
+++ ft_strncmp.c
@@ -6,13 +6,13 @@
 /*   By: gsteyn <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/05/18 09:31:14 by gsteyn            #+#    #+#             */
-/*   Updated: 2018/05/18 09:34:24 by gsteyn           ###   ########.fr       */
+/*   Updated: 2018/05/21 09:16:54 by gsteyn           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-int	strncmp(const char *s1, const char *s2, size_t n)
+int	ft_strncmp(const char *s1, const char *s2, size_t n)
 {
 	while (*s1 && *s2 && n)
 	{
