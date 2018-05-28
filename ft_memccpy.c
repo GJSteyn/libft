d02fcb422156d259164a24c5ec8c51550bbd6517
GJSteyn <6443537+GJSteyn@users.noreply.github.com>
2018-05-28 09:03:58 +0200

    Fix

--- ft_memccpy.c
+++ ft_memccpy.c
@@ -6,7 +6,7 @@
 /*   By: gsteyn <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/05/16 17:03:54 by gsteyn            #+#    #+#             */
-/*   Updated: 2018/05/25 13:23:58 by gsteyn           ###   ########.fr       */
+/*   Updated: 2018/05/28 08:57:16 by gsteyn           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,7 +17,7 @@ void	*ft_memccpy(void *dst, const void *src, int c, size_t n)
 	size_t	i;
 
 	i = 0;
-	while (((char*)src)[i] && i < n)
+	while (i < n)
 	{
 		((unsigned char*)dst)[i] = ((unsigned char*)src)[i];
 		if (((unsigned char*)dst)[i] == (unsigned char)c)
--- ft_strlcat.c
+++ ft_strlcat.c
@@ -6,46 +6,35 @@
 /*   By: gsteyn <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/05/24 09:39:46 by gsteyn            #+#    #+#             */
-/*   Updated: 2018/05/26 15:09:11 by gsteyn           ###   ########.fr       */
+/*   Updated: 2018/05/28 08:03:19 by gsteyn           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-//size_t	ft_strlcat(char *dst, const char *src, size_t dstsize)
-//{
-//	size_t	i;
-//	size_t	dst_len;
-//	size_t	app;
-//
-//	i = 0;
-//	dst_len = ft_strlen(dst);
-//	app = dstsize - dst_len - 1;
-//	while (i < app)
-//	{
-//		dst[dst_len + i] = src[i];
-//		i++;
-//	}
-//	if (!(dstsize == 0 || dst_len > dstsize))
-//		dst[dst_len + i] = '\0';
-//	return (dst_len + i);
-//}
-
-size_t	ft_strlcat(char *dst, const char *src, size_t dstsize)
+size_t	ft_strlcat(char *dst, const char *src, size_t siz)
 {
-	size_t	i;
-	size_t	dst_len;
-	size_t	total_len;
+	char *d = dst;
+	const char *s = src;
+	size_t n = siz;
+	size_t dlen;
 
-	i = 0;
-	dst_len = ft_strlen(dst);
-	total_len = dst_len + ft_strlen(src);
-	
-	while (i < dstsize - dst_len)
-	{
-		dst[dst_len + i] = src[i];
-		i++;
+	/* Find the end of dst and adjust bytes left but don't go past end */
+	while (n-- != 0 && *d != '\0')
+		d++;
+	dlen = d - dst;
+	n = siz - dlen;
+
+	if (n == 0)
+		return(dlen + strlen(s));
+	while (*s != '\0') {
+		if (n != 1) {
+			*d++ = *s;
+			n--;
+		}
+		s++;
 	}
-	dst[i] = '\0';
-	return (total_len);
+	*d = '\0';
+
+	return(dlen + (s - src));	/* count does not include NUL */
 }
