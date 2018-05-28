1d88da208435f1f45ce91735387fe7215b773e2f
GJSteyn <6443537+GJSteyn@users.noreply.github.com>
2018-05-28 13:59:03 +0200

    Minor fix

--- ft_lstdel.c
+++ ft_lstdel.c
@@ -6,7 +6,7 @@
 /*   By: gsteyn <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/05/25 05:33:43 by gsteyn            #+#    #+#             */
-/*   Updated: 2018/05/28 12:34:46 by gsteyn           ###   ########.fr       */
+/*   Updated: 2018/05/28 13:58:34 by gsteyn           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,8 +17,8 @@ void	ft_lstdel(t_list **alst, void (*del)(void *, size_t))
 	t_list *tmp;
 	t_list *hold;
 
-	tmp = *alst;
-	while (tmp->next)
+	tmp = *alst->next;
+	while (tmp)
 	{
 		hold = tmp->next;
 		(*del)(tmp->content, tmp->content_size);
