181c7a6bed51358d07f2e9f0f1bcda2f4e7ed79d
GJSteyn <6443537+GJSteyn@users.noreply.github.com>
2018-05-28 09:04:16 +0200

    Fix

--- ft_strlcat.c
+++ ft_strlcat.c
@@ -6,24 +6,24 @@
 /*   By: gsteyn <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/05/24 09:39:46 by gsteyn            #+#    #+#             */
-/*   Updated: 2018/05/28 08:03:19 by gsteyn           ###   ########.fr       */
+/*   Updated: 2018/05/28 09:03:12 by gsteyn           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-size_t	ft_strlcat(char *dst, const char *src, size_t siz)
+size_t	ft_strlcat(char *dst, const char *src, size_t dstsize)
 {
 	char *d = dst;
 	const char *s = src;
-	size_t n = siz;
+	size_t n = dstsize;
 	size_t dlen;
 
 	/* Find the end of dst and adjust bytes left but don't go past end */
 	while (n-- != 0 && *d != '\0')
 		d++;
 	dlen = d - dst;
-	n = siz - dlen;
+	n = dstsize - dlen;
 
 	if (n == 0)
 		return(dlen + strlen(s));
