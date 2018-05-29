310df30360f24b27177f6914c597e17c1b31e019
GJSteyn <6443537+GJSteyn@users.noreply.github.com>
2018-05-29 14:13:35 +0200

    Fix for moulitest

--- ft_strncmp.c
+++ ft_strncmp.c
@@ -6,7 +6,7 @@
 /*   By: gsteyn <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/05/18 09:31:14 by gsteyn            #+#    #+#             */
-/*   Updated: 2018/05/24 12:43:53 by gsteyn           ###   ########.fr       */
+/*   Updated: 2018/05/29 14:11:27 by gsteyn           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,17 +14,24 @@
 
 int	ft_strncmp(const char *s1, const char *s2, size_t n)
 {
+	int		len;
+
 	if (ft_strlen(s1) == 0 && ft_strlen(s2) == 0)
 		return (0);
 	if (ft_strlen(s1) == 0)
 		return (-1);
 	if (ft_strlen(s2) == 0)
 		return (1);
-	while (*s1 && *s2 && n)
+	if (ft_strlen(s1) >= ft_strlen(s2))
+		len = ft_strlen(s1);
+	else
+		len = ft_strlen(s2);
+	while (len && n)
 	{
 		if (*((unsigned char*)s1) == *((unsigned char*)s2))
 		{
 			n--;
+			len--;
 			s1++;
 			s2++;
 		}
