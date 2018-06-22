8277afcca57b0835348b47d5519a14053303dbd8
GJSteyn <36443537+GJSteyn@users.noreply.github.com>
2018-06-22 16:40:28 +0200

    Remove unnecessary variable in ft_putnstr and ft_putnstr_fd

--- ft_putnstr.c
+++ ft_putnstr.c
@@ -2,12 +2,10 @@
 
 void		ft_putnstr(char *str, size_t n)
 {
-	size_t		i;
 	size_t		len;
 
 	if (str)
 	{
-		i = 0;
 		len = ft_strlen(str);
 		if (len < n)
 			write(1, str, len);
--- ft_putnstr_fd.c
+++ ft_putnstr_fd.c
@@ -2,12 +2,10 @@
 
 void		ft_putnstr_fd(char *str, int fd, size_t n)
 {
-	size_t		i;
 	size_t		len;
 
 	if (str)
 	{
-		i = 0;
 		len = ft_strlen(str);
 		if (len < n)
 			write(fd, str, len);
