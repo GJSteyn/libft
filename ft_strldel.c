ebb90ebfd42eebf0a68b165cd06670a37398d9dc
GJSteyn <36443537+GJSteyn@users.noreply.github.com>
2018-07-09 17:37:55 +0200

    ft_strldel finished

--- ft_strldel.c
+++ ft_strldel.c
@@ -6,7 +6,7 @@
 /*   By: gsteyn <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/07/09 06:43:46 by gsteyn            #+#    #+#             */
-/*   Updated: 2018/07/09 14:00:18 by gsteyn           ###   ########.fr       */
+/*   Updated: 2018/07/09 17:36:23 by gsteyn           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,16 +18,14 @@
 
 void		ft_strldel(char ***str)
 {
+	int		i;
+
+	i = 0;
 	if (*str)
 	{
-		ft_putstr_fd("Before strldel while loop\n", 2);
-		while (**str)
-		{
-			ft_putstr_fd("inside strldel while loop\n", 2);
-			ft_putstr_fd(**str, 2);
-			ft_strdel(*str);
-			(**str)++;
-		}
+		while ((*str)[i])
+			free((*str)[i++]);
+		free(*str);
 		*str = NULL;
 	}
 }
