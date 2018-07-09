2b7abebec653e7bf2e0134cdd560c6877b73fe97
GJSteyn <6443537+GJSteyn@users.noreply.github.com>
2018-07-09 14:03:03 +0200

    Added debugging statements to ft_strldel

--- ft_strldel.c
+++ ft_strldel.c
@@ -6,7 +6,7 @@
 /*   By: gsteyn <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/07/09 06:43:46 by gsteyn            #+#    #+#             */
-/*   Updated: 2018/07/09 10:25:21 by gsteyn           ###   ########.fr       */
+/*   Updated: 2018/07/09 14:00:18 by gsteyn           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,8 +20,11 @@ void		ft_strldel(char ***str)
 {
 	if (*str)
 	{
+		ft_putstr_fd("Before strldel while loop\n", 2);
 		while (**str)
 		{
+			ft_putstr_fd("inside strldel while loop\n", 2);
+			ft_putstr_fd(**str, 2);
 			ft_strdel(*str);
 			(**str)++;
 		}
