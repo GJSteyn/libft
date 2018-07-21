870676580aaa273e010cbe8e89f955ed8ef83763
GJSteyn <6443537+GJSteyn@users.noreply.github.com>
2018-07-21 18:27:04 +0200

    Fix ft_lstdelcontent, but it's still not entirely perfect

--- ft_lstdelcontent.c
+++ ft_lstdelcontent.c
@@ -6,7 +6,7 @@
 /*   By: gsteyn <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/07/21 18:08:28 by gsteyn            #+#    #+#             */
-/*   Updated: 2018/07/21 18:13:40 by gsteyn           ###   ########.fr       */
+/*   Updated: 2018/07/21 18:26:13 by gsteyn           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,4 +15,5 @@
 void		ft_lstdelcontent(void *content, size_t size)
 {
 	ft_memdel(&content);
+	size = 0;
 }
