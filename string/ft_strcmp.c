febc9e14918982e72265efe38d9c0ba910d352f8
GJSteyn <36443537+GJSteyn@users.noreply.github.com>
2018-09-25 10:55:38 +0200

    Improve performance of f_strcmp

--- string/ft_strcmp.c
+++ string/ft_strcmp.c
@@ -6,7 +6,7 @@
 /*   By: gsteyn <gsteyn@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/05/18 08:49:00 by gsteyn            #+#    #+#             */
-/*   Updated: 2018/09/25 09:24:18 by gsteyn           ###   ########.fr       */
+/*   Updated: 2018/09/25 10:54:30 by gsteyn           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,19 +14,20 @@
 
 int	ft_strcmp(const char *s1, const char *s2)
 {
-	size_t		len;
+	size_t		len1;
+	size_t		len2;
 
-	if (ft_strlen(s1) == 0 && ft_strlen(s2) == 0)
+	len1 = ft_strlen(s1);
+	len2 = ft_strlen(s2);
+	if (len1 == 0 && len2 == 0)
 		return (0);
-	if (ft_strlen(s1) == 0)
+	if (len1 == 0)
 		return (-1);
-	if (ft_strlen(s2) == 0)
+	if (len2 == 0)
 		return (1);
-	if (ft_strlen(s1) > ft_strlen(s2))
-		len = ft_strlen(s1);
-	else
-		len = ft_strlen(s2);
-	while (len--)
+	if (len1 > len2)
+		len2 = len1;
+	while (len2--)
 	{
 		if (*((unsigned char*)s1) == *((unsigned char*)s2))
 		{
